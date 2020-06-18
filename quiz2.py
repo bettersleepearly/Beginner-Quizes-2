a280b39abfbe5d40f499b9725a233a3a631cde92
Jack Tran (bettersleepearly) <anhviet3326@gmail.com>
2020-06-18 15:51:16 -0400

    Add files via upload

---
+++ quiz2.py
@@ -0,0 +1,10 @@
+i = n + 1
+squareroot = {int(range(1, i)): int(math.sqrt(range(1, i)))}
+print('So hey name plz:')
+print('\n___________________________________________________________________________________________________________________________________________________')
+name = str(input())
+print('Oh hey' + ' ' + name+ ' ' + ', I should stop fuckin asking for name :)')
+print('\nAnyway, here u go (QUIZ 3):\nTo find square root of numbers from 1 to (n)...')
+n = int(input('\nGive (n) a number:')
+for outcome in squareroot:
+    print(squareroot)
---
+++ quiz3.py
@@ -0,0 +1,7 @@
+
+income = str(input('So...I guess, give me some random numbers or words now (please include "," between each variable):'))
+outcome = income.split(',')
+tuplez = tuple(outcome)
+
+print(outcome)
+print(tuplez)
