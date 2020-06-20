8e917679cab116f1564a0ee60b30bff682184fa9
Jack Tran (bettersleepearly) <anhviet3326@gmail.com>
2020-06-20 10:17:51 -0400

    Add files via upload

---
+++ quiz2.py
@@ -0,0 +1,23 @@
+n = int(input('Back to quiz2 bitch, give me a number so i can squareroot this bitch from 1 to it: ')) #good, no prob
+
+my_dict = {}
+#for i in range(1,num):
+    #my_dict[i] = i*i
+i = 0
+while i < n:
+    i += 1
+    my_dict[i] = i*i
+print(my_dict)    
+
+# so u only use range for the for loop? not really but yeah usually # kay i got this :)) 
+# for the next quizzes if i need to use loop i will try both
+
+# im not sweating i just felt relief af :))) finally got it
+# ur a dumb ass but ur doing fine dont sweat
+# good
+# do the next quiz then we will move on to nested loop # wtf nested loop exist? :)))) fuckckkckckckckckcck
+# which is double the fun
+# yayyy
+# gonna go now
+# upload to git and do the next one # kay # kay bitch ill send them to u later
+# aim for 20mins max
