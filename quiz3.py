6803729424f940132549056aea946e9b9cec4a95
Jack Tran (bettersleepearly) <anhviet3326@gmail.com>
2020-06-23 13:34:55 -0400

    Delete quiz3.py

--- quiz3.py
+++
@@ -1,7 +0,0 @@
-
-income = str(input('So...I guess, give me some random numbers or words now (please include "," between each variable):'))
-outcome = income.split(',')
-tuplez = tuple(outcome)
-
-print(outcome)
-print(tuplez)
