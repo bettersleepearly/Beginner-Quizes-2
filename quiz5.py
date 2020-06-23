11b345c0338a0d0f09657e39d768563c74538f7f
Jack Tran (bettersleepearly) <anhviet3326@gmail.com>
2020-06-23 13:35:16 -0400

    Delete quiz5.py

--- quiz5.py
+++
@@ -1,7 +0,0 @@
-#income = str(input('Hey bitch, no give me some sentences that I can capitalize by some reason :) :'))
-#capitalized_income = " ".join([word.capitalize() for word in income.split(" ")])
-#print(capitalized_income)
-
-income = str(input('Hey bitch, no give me some sentences that I can capitalize by some reason :) :'))
-
-print(income.upper())
