9e9838191eb99df33aedb9cc6a65959672ffd66a
Jack Tran (bettersleepearly) <anhviet3326@gmail.com>
2020-06-23 13:34:16 -0400

    Delete quiz1.py

--- quiz1.py
+++
@@ -1,9 +0,0 @@
-print('Hi!!!\nSee now, I know your name is ADAM :)')
-intro = str(input('\nRight? If yes type "fuckyea", if no type "byebitch" :):'))
-print('___________________________________________________________________________________________________________________________________________________')
-if intro == "byebitch":
-    print('Oh... Well shit :)')
-if intro == "fuckyea":
-    for number in range(2000, 3200):
-        if(number % 7 == 0, number % 5 != 0):
-            print(number)
