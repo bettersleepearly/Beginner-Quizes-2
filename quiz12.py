4be47f669999c219bc1a86b70d7d9ee96e3cc7df
Jack Tran (bettersleepearly) <anhviet3326@gmail.com>
2020-07-02 12:11:01 -0400

    Add files via upload

---
+++ quiz12.py
@@ -0,0 +1,7 @@
+print("\nQuiz 12, draw a square tringale with fucking money by inserting a number for the tringale sides'length.")
+n = int(input("\nNumber bitch:"))
+i = 0
+money = "$"
+for i in range(0,n):
+    i += 1
+    print((money + " ")*i)
